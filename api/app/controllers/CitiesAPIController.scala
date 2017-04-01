44ff5405f5a30469d770b1ff8685f7bbaf2f88ef
Andrii Malaman <and.malaman@gmail.com>
2017-04-01 11:33:56 +0200

    - api/controllers methods refactoring

--- api/app/controllers/CitiesAPIController.scala
+++ api/app/controllers/CitiesAPIController.scala
@@ -12,10 +12,18 @@ import scala.util.Properties
 
 class CitiesAPIController @Inject() (ws: WSClient) extends Controller {
     val API_KEY = Properties.envOrElse("GOOGLE_API_KEY", "GOOGLE_API_KEY");
-    def getCitySuggestions = Action.async {
-        val city = "Paris"
-      ws.url(s"https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${city}&types=geocode&key=${API_KEY}").get().map { response =>
-        Ok(response.body)
-      }
+
+    def getCities(city: String): Future[WSResponse] = {
+        ws.url(s"https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${city}&types=geocode&key=${API_KEY}").get()
+    }
+
+    def getCitySuggestions(city: String) = Action.async { request =>
+        val city = request.getQueryString("city").mkString
+        val citiesFuture = getCities(city);
+        citiesFuture.map {
+            case response => {
+                Ok(response.body)
+            }
+        }
     }
 }
--- api/app/controllers/WeatherController.scala
+++ api/app/controllers/WeatherController.scala
@@ -9,13 +9,17 @@ import ExecutionContext.Implicits.global
 import javax.inject.{Inject, Singleton}
 import scala.util.Properties
 
-
 class WeatherController @Inject() (ws: WSClient) extends Controller {
+    val API_KEY = Properties.envOrElse("WEATHER_API_KEY", "WEATHER_API_KEY")
 
-    def getWeatherForCity = Action.async {
-        val API_KEY = Properties.envOrElse("WEATHER_API_KEY", "WEATHER_API_KEY");
-        val city = "Hamburg";
-        ws.url(s"http://samples.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}").get().map { response =>
+    def getWeather(city: String): Future[WSResponse] = {
+        val url = s"http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}"
+        ws.url(url).get()
+    }
+
+    def getWeatherForCity(city: String) = Action.async { request =>
+        val city = request.getQueryString("city").mkString
+        getWeather(city).map { response =>
             Ok(response.body)
         }
     }
