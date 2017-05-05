c9aaf134760b58e06187469b0b163241706457dc
Andrii Malaman <and.malaman@gmail.com>
2017-05-05 09:05:27 +0200

    - add CORSFilter class

--- frontend/app/main.jsx
+++ frontend/app/main.jsx
@@ -1,6 +1,9 @@
+import 'isomorphic-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+
+require('es6-promise').polyfill();
 
 ReactDOM.render(
   <App />,
