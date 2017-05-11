135fad695a463a63664256a834f396cfb8fc5d14
Andrii Malaman <and.malaman@gmail.com>
2017-05-11 10:11:01 +0200

    - commit services folder

---
+++ api/app/services/CitiesService.scala
@@ -0,0 +1,18 @@
+package services
+
+import javax.inject._
+import play.api.libs.functional.syntax._
+import javax.inject.{Inject, Singleton}
+import play.api.libs.ws._
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import scala.util.Properties
+
+@Singleton
+class CitiesService @Inject() (ws: WSClient) {
+    val API_KEY = Properties.envOrElse("GOOGLE_API_KEY", "GOOGLE_API_KEY");
+
+    def getCities(city: String): Future[WSResponse] = {
+        ws.url(s"https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${city}&types=geocode&key=${API_KEY}").get()
+    }
+}
---
+++ api/app/services/WeatherService.scala
@@ -0,0 +1,19 @@
+package services
+
+import javax.inject._
+import play.api.libs.functional.syntax._
+import javax.inject.{Inject, Singleton}
+import play.api.libs.ws._
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import scala.util.Properties
+
+@Singleton
+class WeatherService @Inject() (ws: WSClient) {
+    val API_KEY = Properties.envOrElse("WEATHER_API_KEY", "WEATHER_API_KEY")
+
+    def getWeather(city: String): Future[WSResponse] = {
+        val url = s"http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}"
+        ws.url(url).get()
+    }
+}
