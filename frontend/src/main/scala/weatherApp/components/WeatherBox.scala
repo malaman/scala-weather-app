3a7b01c2192f5c0c331660af36bd9ad1ea271331
Andrii Malaman <and.malaman@gmail.com>
2017-09-08 10:24:26 +0200

    - add save city to favorites, remove city from favorites buttons to WeatherBox component (frontend)

--- frontend/src/main/scala/weatherApp/components/WeatherBox.scala
+++ frontend/src/main/scala/weatherApp/components/WeatherBox.scala
@@ -3,82 +3,128 @@ package weatherApp.components
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 import japgolly.scalajs.react.extra.router.RouterCtl
-import weatherApp.models.{OpenWeatherBaseCity, UserResponse, WeatherResponse}
+import org.scalajs.dom
+import weatherApp.config.Config
+import weatherApp.models.{CityForUser, OpenWeatherBaseCity, UserResponse, WeatherResponse}
 import weatherApp.router.AppRouter
+import io.circe.syntax._
+import io.circe.generic.auto._
 
 object WeatherBox {
 
   case class Props (
     weather: Option[WeatherResponse],
     ctl: RouterCtl[AppRouter.Page],
-    userInfo: Option[UserResponse]
+    userInfo: Option[UserResponse],
+    isRemoveBtn: Boolean = false
   )
-  val Component = ScalaFnComponent[Props](props => {
-    if (props.weather.isDefined) {
-      val weather = props.weather.get
-      val userInfoOption = props.userInfo
-      val iconStr = s"wi-owm-${weather.weather.head.id}"
-      val link: TagMod = props.ctl.link(AppRouter.CityRoute(weather.name.toLowerCase(), weather.id))
-      val city = OpenWeatherBaseCity(
-        id = weather.id,
-        name = weather.name,
-        lat = weather.coord.lat,
-        lon = weather.coord.lon
-      )
-      <.div(
-        ^.cls := "weather-box",
-        ^.maxWidth := "400px",
-        ^.display := "flex",
-        ^.border := "1px solid",
-        ^.color := "black",
-        <.div(
-          ^.margin := "5px",
-          ^.display := "flex",
-          ^.flexDirection := "row",
-          ^.justifyContent := "space-between",
-          ^.width := "100%",
-          ^.fontSize := 15.px,
-          <.div(
-            <.div(weather.name),
-            <.div(
-              s"${(math rint weather.main.temp * 10) / 10} °C"
-            )
-          ),
-          <.div(
-            ^.display := "flex",
-            ^.justifyContent := "center",
-            ^.alignItems := "center",
-            <.div(
-              ^.className := s"wi $iconStr",
-              ^.marginRight := "10px",
-              ^.fontSize := 25.px,
-              ^.fontWeight := "100"
-            ),
-            <.div(
-              weather.weather.head.main
-            )
-          ),
-          <.div(
 
-          ),
+  class Backend(bs: BackendScope[Props, Unit]) {
+    val host: String = Config.AppConfig.apiHost
+
+    def addCityForUser(city: OpenWeatherBaseCity, userId: Int): Callback = {
+      val cityForUser = CityForUser(city, userId).asJson.asInstanceOf[dom.ext.Ajax.InputData]
+      Callback {
+        dom.ext.Ajax.post(
+          url = s"$host/city",
+          data = cityForUser,
+          headers = Map("Content-Type" -> "application/json")
+        )
+      }
+    }
+
+    def removeCityForUser(city: OpenWeatherBaseCity, userId: Int): Callback = {
+      val cityForUser = CityForUser(city, userId).asJson.asInstanceOf[dom.ext.Ajax.InputData]
+      Callback {
+        dom.ext.Ajax.delete(
+          url = s"$host/city",
+          data = cityForUser,
+          headers = Map("Content-Type" -> "application/json")
+        )
+      }
+    }
+
+    def navigateToCityPage(page: AppRouter.CityRoute): Callback = bs.props.flatMap { props =>
+      props.ctl.set(page)
+    }
+
+    def render(props: Props): VdomElement = {
+      <.div(
+        props.weather.map(weather => {
+          val userInfoOption = props.userInfo
+          val iconStr = s"wi-owm-${weather.weather.head.id}"
+          val city = OpenWeatherBaseCity(
+            id = weather.id,
+            name = weather.name,
+            lat = weather.coord.lat,
+            lon = weather.coord.lon
+          )
           <.div(
+            ^.cls := "weather-box",
+            ^.maxWidth := "400px",
             ^.display := "flex",
-            ^.flexDirection := "row",
-            ^.justifyContent := "center",
-            ^.alignItems := "center",
+            ^.border := "1px solid",
+            ^.color := "black",
+            <.div(
+              ^.margin := "5px",
+              ^.display := "flex",
+              ^.flexDirection := "row",
+              ^.justifyContent := "space-between",
+              ^.width := "100%",
+              ^.fontSize := 15.px,
+              <.div(
+                <.div(weather.name),
+                <.div(
+                  s"${(math rint weather.main.temp * 10) / 10} °C"
+                )
+              ),
+              <.div(
+                ^.display := "flex",
+                ^.justifyContent := "center",
+                ^.alignItems := "center",
+                <.div(
+                  ^.className := s"wi $iconStr",
+                  ^.marginRight := "10px",
+                  ^.fontSize := 25.px,
+                  ^.fontWeight := "100"
+                ),
+                <.div(
+                  weather.weather.head.main
+                )
+              )
+            ),
             <.div(
               ^.display := "flex",
+              ^.flexDirection := "row",
               ^.justifyContent := "center",
               ^.alignItems := "center",
-              WeatherBoxBtn(WeatherBoxBtn.Props(link))
-            ),
-            userInfoOption.map(userInfo => AddCityBtn(AddCityBtn.Props(city, userInfo.user.id))).whenDefined,
+              ^.padding := 5.px,
+              WeatherBoxBtn(
+                WeatherBoxBtn.Props(
+                  "more",
+                  navigateToCityPage(AppRouter.CityRoute(weather.name.toLowerCase(), weather.id))
+                )
+              ),
+              userInfoOption.map(userInfo =>
+                <.div(
+                  ^.display := "flex",
+                  WeatherBoxBtn(
+                    WeatherBoxBtn.Props("save", addCityForUser(city, userInfo.user.id))
+                  ),
+                  WeatherBoxBtn(
+                    WeatherBoxBtn.Props("remove", removeCityForUser(city, userInfo.user.id))
+                  ).when(props.isRemoveBtn)
+                )
+              ).whenDefined
+            )
           )
-        )
+        }).whenDefined
       )
-    } else {
-      <.div()
     }
-  })
+  }
+
+  val Component = ScalaComponent.builder[Props]("WeatherBox")
+    .renderBackend[Backend].build
+
   def apply(props: Props) = Component(props)
 }
--- frontend/src/main/scala/weatherApp/components/WeatherBoxBtn.scala
+++ frontend/src/main/scala/weatherApp/components/WeatherBoxBtn.scala
@@ -4,23 +4,28 @@ import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 
 object WeatherBoxBtn {
-  case class Props (link: TagMod)
-  val Component = ScalaFnComponent[Props](props => {
+  case class Props(
+                    label: String,
+                    onClick: Callback
+                  )
+
+  val Component = ScalaFnComponent[Props]{ props =>
     <.div(
-      props.link(
-        <.button(
-          ^.cls := "primary-hover",
-          ^.marginLeft := 5.px,
-          ^.border := "none",
-          ^.cursor := "pointer",
-          ^.color := "white",
-          ^.padding := "5px 10px",
-          ^.borderRadius := "2px",
-          "more"
-        )
+      <.button(
+        ^.cls := "secondary-hover",
+        ^.marginLeft := 5.px,
+        ^.border := "none",
+        ^.cursor := "pointer",
+        ^.color := "black",
+        ^.padding := "5px 10px",
+        ^.borderRadius := "2px",
+        ^.backgroundColor := "white",
+        ^.border := "1px solid black",
+        ^.onClick --> props.onClick,
+        props.label
       )
     )
-  })
+  }
 
   def apply(props: Props) = Component(props)
 }
